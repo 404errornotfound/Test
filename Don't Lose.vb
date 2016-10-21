3163216a9377148c6d40c8e1bfcf7bb4e4a45405
404errornotfound <kerbalspacemail@gmail.com>
2016-10-21 14:00:31 +1100

    Create Don't Lose.vb

---
+++ Don't Lose.vb
@@ -0,0 +1,9 @@
+Imports
+System
+Public Class Nope
+Dim 0 As String = "You go right"
+Dim 1 As String = "You go left"
+Dim 2 As String = "You fell off the face of the earth"
+Dim 3 As String = "You try to scream in space, but no one can hear you"
+Dim 4 As String = "GAME OVER (That's it, what were you expecting?)"
+Dim 5 As String = "You saw a dog try to eat a lemon. (Thin)"
